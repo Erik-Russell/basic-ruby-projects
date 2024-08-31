a951d17178110bd11fff188a0cced99bffb88189
Erik Russell <erik.z.russell@gmail.com>
2024-08-30 18:07:15 -0700

    Brainstorming roughing out psuedocode

--- stock-picker/main.rb
+++ stock-picker/main.rb
@@ -1,3 +1,27 @@
 # RESEARCH AND NOTES
 # found Kadane Algorithm for finding the max sum of a subarray,
 # I think this could be a really good starting point
+
+# https://github.com/kumar91gopi/Algorithms-and-Data-Structures-in-Ruby/blob/master/arrays/MaxDiffLargerOnRight.rb
+# ^^^^ this is an example written in ruby, but it only finds the maximum sum
+
+# Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
+
+#   > stock_picker([17,3,6,9,15,8,6,1,10])
+#   => [1,4]  # for a profit of $15 - $3 == $12
+
+# Quick Tips:
+
+#     You need to buy before you can sell
+#     Pay attention to edge cases like when the lowest day is the last day or the highest day is the first day.
+
+
+
+### my thoughts
+# set each number in array as a value in a hash with each values key being its index + 1
+# scan through all numbers
+# -> if the smallest number is the last number, ignore it
+# using each number except for the last number, calc the difference between it and every other number and keep the largest diff
+# --> only compare numbers with a greater index
+# compare each numbers larget individual differences and find the largest difference
+# return an array of pairs indexes
