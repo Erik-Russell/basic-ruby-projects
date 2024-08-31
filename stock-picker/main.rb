d71571789bf96baf183d4d914056114c0ca1698d
Erik Russell <erik.z.russell@gmail.com>
2024-08-31 10:34:24 -0700

    Finished Version 1.1 removed unused variable array

--- stock-picker/main.rb
+++ stock-picker/main.rb
@@ -2,7 +2,6 @@ def stock_picker (stocks)
   buy_value = stocks[0] #min_value
   buy_stock_index = 0 #min_index
   global_profit = 0  #max_difference
-  buy_and_sell = [] #pair of values
   sell_stock_index = 1 #greater_number_index
   pair = [] #pair of indexes
 
@@ -11,7 +10,6 @@ def stock_picker (stocks)
     current_profit = current_value - buy_value 
     if current_profit > global_profit
       global_profit = current_profit
-      buy_and_sell = [buy_value, current_value]
       sell_stock_index = index
       buy_stock_index = stocks.index(buy_value) #track buy value index when we have a change to global_profit
     end
