4ff8d97c1326edb52fe0145f01b2de6c000a1625
Erik Russell <erik.z.russell@gmail.com>
2024-08-31 10:14:10 -0700

    Second Draft code now will track and can return index of stock values

--- stock-picker/main.rb
+++ stock-picker/main.rb
@@ -26,27 +26,41 @@
 # compare each numbers larget individual differences and find the largest difference
 # return an array of pairs indexes
 
-stocks = [17,3,6,9,15,8,6,1,10]
+stocks = [8,17,3,6,9,15,8,6,1,10]
 
-buy_stock = stocks[0]
-profit = 0
-buy_and_sell = []
+buy_value = stocks[0] #min_value
+buy_stock_index = 0 #min_index
+profit = 0  #max_difference
+buy_and_sell = [] #pair
+sell_stock_index = 1 #greater_number_index
 
 stocks.each_with_index do |current_value, index|
-  next if index == 0 # Skip the first element since it's already assigned to buy_stock
+  next if index == 0 # Skip the first element since it's already assigned to buy_value
+
+  puts "cv: #{current_value} - bs: #{buy_value}"
 
   # Calculate the difference between the current value and the minimum value seen so far
-  difference = current_value - buy_stock
-  
+  difference = current_value - buy_value
+  puts "diff: #{difference} :: profit: #{profit}"
+
   # If the current difference is greater than the maximum difference found so far, update the maximum difference and buy_and_sell
   if difference > profit
     profit = difference
-    buy_and_sell = [buy_stock, current_value]
+    buy_and_sell = [buy_value, current_value]
+    sell_stock_index = index + 1
+    puts "buy and sell: #{buy_and_sell}"
+    buy_stock_index = stocks.index(buy_value) + 1
   end
+  puts "sell_stock_index: #{sell_stock_index}"
+  
 
   # Update the minimum value seen so far if the current value is less than the minimum value
-  buy_stock = [buy_stock, current_value].min
+  buy_value = [buy_value, current_value].min
+  puts "buy index: #{buy_stock_index}"
+  puts "current buy stock: #{buy_value}"
+  puts
 end
 
 puts "Pair with the greatest difference: #{buy_and_sell}"
+puts 
 puts "Profit: #{profit}"
