8d1cc3bc3232dccf3d8844d57248958b7366799c
Erik Russell <erik.z.russell@gmail.com>
2024-09-01 07:11:14 -0700

    Finished Version bubble sort algorithm function check SAT tested against multiple arrays found did not properly sort repeat numbers => changed number comparison logic from > to >=

--- bubble-sort/main.rb
+++ bubble-sort/main.rb
@@ -1,19 +1,36 @@
 def bubble_sort (array)
-  array.each_with_index do |number, index|
-    if array.last == number
-      puts "break"
+  length = array.length
+
+  for _ in 1..length do
+    swapped = false
+
+    array.each_with_index do |number, index|
+      if array.last == number
+        break
+      end
+      if index == array.length
+        next
+      end
+      if number >= array[index+1]
+        array[index], array[index + 1] = array[index + 1], array[index]
+        swapped = true
+      end
+    end
+    if swapped == false
       break
     end
-    puts "start of #{index} loop"
-    if index == array.length
-      puts "length #{array.length}"
-      next
-    end
-    if number > array[index+1]
-      puts "swap #{number} for #{array[index+1]}"
-      array[index], array[index + 1] = array[index + 1], array[index]
-    end
   end
+  p array
 end
 
-puts bubble_sort([5,3,8,6,55,0,1])
+bubble_sort([5,3,8,6,55,0,1])
+
+#test
+
+# test_array_1 = 50.times.map { rand(1..50) }
+# test_array_2 = 10.times.map { rand(1..50) }
+# test_array_3 = 10.times.map { rand(1..50) }
+
+# bubble_sort(test_array_1)
+# bubble_sort(test_array_2)
+# bubble_sort(test_array_3)
